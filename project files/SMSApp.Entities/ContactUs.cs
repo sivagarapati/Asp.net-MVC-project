3fd96a1faa4e3332473db1d185a52fe83dcd848e
sivagarapati <siva.gsk333@gmail.com>
2017-04-01 06:09:05 +0530

    Add files via upload

---
+++ project files/SMSApp.Entities/ContactUs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSApp.Entities
+{
+    public class ContactUs
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        [RegularExpression(@"^(\d{10})$", ErrorMessage = "Please enter valid phone number.")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Information { get; set; }
+
+    }
+}
